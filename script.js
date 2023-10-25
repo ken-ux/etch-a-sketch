10bce0a16c6b2b179f6b7a3894a727a8d318072d
Kenny Nguyen <sokenns@gmail.com>
2023-10-25 01:00:09 -0700

    Let user click button to choose grid size

--- script.js
+++ script.js
@@ -1,19 +1,51 @@
 const container = document.querySelector(".container");
+const button = document.querySelector("#btn");
 
-for (i = 0; i < 256; i++) {
-  let div = document.createElement("div");
-  div.classList.add("square");
-  container.appendChild(div);
+button.addEventListener("click", userGeneratedGrid);
+
+function userGeneratedGrid() {
+  let num = Number(prompt("How many squares do you want?"));
+  console.log(num);
+  if (isNaN(num) || num === 0) {
+    console.log("Please input a number.");
+  } else if (num > 100) {
+    console.log(
+      "The maximum number of squares is 100. Please choose another number."
+    );
+  } else {
+    clearGrid()
+    createGrid(num);
+    addEventListeners();
+  }
 }
 
-const squares = document.querySelectorAll(".square");
-squares.forEach((square) => {
-  square.addEventListener("mouseover", () => {
-    square.style.backgroundColor = "blue";
-  });
-  square.addEventListener("mouseout", () => {
-    square.style.backgroundColor = "";
-  });
-});
+function createGrid(num) {
+  for (i = 0; i < num; i++) {
+    let div = document.createElement("div");
+    div.classList.add("square");
+    container.appendChild(div);
+  }
+}
 
-console.log(document.querySelectorAll(".square"));
+function addEventListeners() {
+  let squares = document.querySelectorAll(".square");
+  squares.forEach((square) => {
+    square.addEventListener("mouseover", () => {
+      square.style.backgroundColor = "blue";
+    });
+    square.addEventListener("mouseout", () => {
+      square.style.backgroundColor = "";
+    });
+  });
+}
+
+function clearGrid() {
+  let squares = document.querySelectorAll(".square");
+  squares.forEach((square) => {
+    square.remove();
+  });
+}
+
+// Initial grid on page load
+createGrid(10);
+addEventListeners();
