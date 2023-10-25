285c9562f7a1f885209d3e17a407167546b26f2e
Kenny Nguyen <sokenns@gmail.com>
2023-10-25 00:38:34 -0700

    Add mouse over behavior to change background color

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const container = document.querySelector(".container");
 
-for (i = 0; i < 16; i++) {
-    let div = document.createElement("div");
-    div.classList.add("child");
-    container.appendChild(div);
+for (i = 0; i < 256; i++) {
+  let div = document.createElement("div");
+  div.classList.add("square");
+  container.appendChild(div);
 }
 
-console.log(document.querySelectorAll(".child"));
+const squares = document.querySelectorAll(".square");
+squares.forEach((square) => {
+  square.addEventListener("mouseover", () => {
+    square.style.backgroundColor = "blue";
+  });
+  square.addEventListener("mouseout", () => {
+    square.style.backgroundColor = "";
+  });
+});
+
+console.log(document.querySelectorAll(".square"));
